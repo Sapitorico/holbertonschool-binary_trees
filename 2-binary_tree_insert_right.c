cf2b95146117a85960e2c7200276d09132a1d29a
Sapitorico <renzoyaques@gmail.com>
2023-01-11 10:06:20 -0300

    function that add a new node in right pointer to tree

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - adds a new node to the right of the tree
+ *
+ * @parent: father node
+ * @value: value
+ * Return: The new node or null in case of fault
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *New_Node = NULL;
+
+	if (!parent)
+		return (NULL);
+	New_Node = binary_tree_node(parent, value);
+	if (!New_Node)
+		return (NULL);
+	if (parent->right)
+	{
+		New_Node->right = parent->right;
+		parent->right->parent = New_Node;
+	}
+	parent->right = New_Node;
+	return (New_Node);
+}
