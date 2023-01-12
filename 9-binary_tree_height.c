2c05a6eaa366eba51636bcb7c5fb983a2ca916d2
Sapitorico <renzoyaques@gmail.com>
2023-01-12 13:40:41 -0300

    fix a left and height is

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	if (left_height > right_height)
+	if (left_height >= right_height)
 		return (left_height);
 	else
 		return (right_height);
