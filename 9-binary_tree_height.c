9771f34a77646be5c899cc9f03ff31b182f2eaaf
Sapitorico <renzoyaques@gmail.com>
2023-01-12 13:37:46 -0300

    fix a return height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,7 +15,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	if (left_height > right_height)
-		return (left_height + 1);
+		return (left_height);
 	else
-		return (right_height + 1);
+		return (right_height);
 }
