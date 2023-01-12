cd8e8067dade3956f75383afa504626b57ceeb70
Sapitorico <renzoyaques@gmail.com>
2023-01-12 13:33:42 -0300

    line over 80 characters (81)

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,10 +1,9 @@
 #include"binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a tree using post-order traversal
  * @tree: pointer to the root node
  * @func: pointer to a function to call
- *
  *
  * Return: void
  */
