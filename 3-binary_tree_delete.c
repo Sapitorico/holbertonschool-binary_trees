34b65dc1027207348c2e57274d0d8679ef02ca56
Sapitorico <renzoyaques@gmail.com>
2023-01-12 13:17:38 -0300

    fix error code indent should use tab in line 14

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,10 +7,9 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return;
-
 	binary_tree_delete(tree->left);
-        binary_tree_delete(tree->right);
+	binary_tree_delete(tree->right);
 	free(tree);
 }
