75ab9296cc86444a494b2dc64c41f86294b2dcc2
Sapitorico <renzoyaques@gmail.com>
2023-01-12 14:45:01 -0300

    qwdqw

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,6 +14,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
+	left_height + 1;
+	right_height + 1;
 	if (left_height >= right_height)
 		return (left_height);
 	else
