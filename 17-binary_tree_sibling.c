ee54683890ca7b71e594a909448c7cc1f9cbf60e
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-14 16:20:28 -0300

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_t - finds the sibling of a node
+ * @node: node
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
