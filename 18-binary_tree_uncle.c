3680048445c0a71db10d3a25405f728db0d84e1b
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-01-14 16:30:01 -0300

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: node
+ * Return: int
+ */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent || !node->parent->parent)
